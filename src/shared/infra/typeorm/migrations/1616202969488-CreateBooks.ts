b8b8b504cc2738774e7142af01b8820aa717a1ee
xthiagoSant <uzmaki_thiago@outlook.com>
2021-03-19 22:25:57 -0300

    migration table books

---
+++ src/shared/infra/typeorm/migrations/1616202969488-CreateBooks.ts
@@ -0,0 +1,61 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateBooks1616202969488 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'books',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'author',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'isbn',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'publication_year',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'pages',
+                        type: 'integer',
+                        default: 0,
+                    },
+                    {
+                        name: 'synopsis',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'status',
+                        type: 'integer',
+                        default: 1,
+                    }                    
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('books')
+    }
+
+}
