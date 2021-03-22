4a3b461f3d41ac0d7b7a383d98f237e1a60b73d1
xthiagosant <uzmaki_thiago@outlook.com>
2021-03-22 05:59:48 -0300

    criacao path service e modificação da interface para implementacao

--- src/modules/books/repositories/IBooksRepository.ts
+++ src/modules/books/repositories/IBooksRepository.ts
@@ -5,4 +5,5 @@ export interface IBooksRepository{
     findById(id: string): Promise<Books | undefined>
     create(book: ICreateBooksDTO): Promise<Books>
     update(book: ICreateBooksDTO): Promise<Books>
+    delete(id: string): Promise<void>
 }
