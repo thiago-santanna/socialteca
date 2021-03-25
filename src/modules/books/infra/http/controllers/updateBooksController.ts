08a405f5c6aca0887c5af4c496d4a1009844544e
xthiagosant <uzmaki_thiago@outlook.com>
2021-03-25 08:09:27 -0300

    Rotpara atualizar um livro está feita

--- src/modules/books/infra/http/controllers/updateBooksController.ts
+++ src/modules/books/infra/http/controllers/updateBooksController.ts
@@ -1,7 +1,33 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 import { classToClass } from 'class-transformer'
+import { IUpdateBooksDto } from '../../../dto/IBooksDTO'
+
+import UpdateUserService from '../../../service/UpdateBookService'
 
 export default class updateBooksController{
-    
+    public async update(req: Request, res: Response): Promise<Response>{
+        const {
+            id,
+            name, 
+            author, 
+            isbn, 
+            publication_year, 
+            pages, 
+            synopsis
+        }:IUpdateBooksDto = req.body
+
+        const updateBooksService = container.resolve(UpdateUserService)
+        const book = await updateBooksService.execute({
+            id,
+            name, 
+            author, 
+            isbn, 
+            publication_year, 
+            pages, 
+            synopsis
+        })
+
+        return res.json(classToClass(book))
+    }
 }
--- src/modules/books/infra/http/routes/books.routes.ts
+++ src/modules/books/infra/http/routes/books.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express'
 import ensureAuthenticated from '../../../../../shared/infra/http/middlewares/ensureAuthenticated'
 import CreateBooksController from '../controllers/CreateBooksController'
+import UpdateBooksController from '../controllers/UpdateBooksController'
 
 const booksRoutes = Router()
 
 const createBooksController = new CreateBooksController()
+const updateBooksController = new UpdateBooksController()
 
 booksRoutes.post(
     '/', 
     ensureAuthenticated, 
     createBooksController.create
+)
+
+booksRoutes.put(
+    '/',
+    ensureAuthenticated,
+    updateBooksController.update
 )
 
 
